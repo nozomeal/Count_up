e64f4e91805bb4e66392dfe44cfd8d6f07bff5de
Nozomi Sakamoto <nozomeal7@Nozomi-no-MacBook-Air.local>
2015-06-06 15:57:57 +0900

    first commit

--- Count66/ViewController.swift
+++ Count66/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var number: Int = 0
+    @IBOutlet var label : UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func   plus(){
+    number = number + 1
+    label.text = String(number)
+    }
+    
+    
 }
 
